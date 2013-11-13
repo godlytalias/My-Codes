444d311f0763e2dd9f25c48f852b8284a2d34b1e
Godly T.Alias <godlytalias@yahoo.co.in>
2013-11-13 15:03:57 +0530

    Spiral Matrix

--- spiral matrix.cpp
+++ spiral matrix.cpp
@@ -18,37 +18,46 @@ Copyright (c) 2013 GODLY T.ALIAS
 
 
 
+//This is program for spiral matrix. Spiral Matrix is a matrix with
+//the middle element as 0 and will be incremented by 1 as it goes out spirally
+//eg: n=8
+//           4 3 2
+//           5 0 1
+//           6 7 8
+
+
 #include<iostream>
 #include<cstdlib>
 #include<iomanip>
 using namespace std;
 int n;
-
+ 
+//returns the direction in which the next element to be filled up in the matrix
 int direction(int i, int j, int key)
 {
 if((abs((n/2)-i)==key) && (abs((n/2)-j)==key))
 {
  if(i<(n/2) && j>(n/2))
-        return 1;
+        return 1;//left
  else if(i<(n/2) && j<(n/2))
-        return 2;
+        return 2;//down
  else if(i>(n/2) && j<(n/2))
-        return 3;
+        return 3;//right
  else return -1;
                      }
  else if((abs((n/2)-i)==key) && (abs((n/2)-j)>key) && i>(n/2) && j>(n/2))
-      return 0;
-      
+      return 0;//up
+ 
  else return -1;
 }
-
+ 
 int main(){
     int i,j,count=1,temp;
-    cout<<"Enter n(<19) : ";
+    cout<<"Enter n (<50): ";
     cin>>n;
-    
-int sp[20][20],key,dir;
-
+     
+int sp[50][50],key,dir;
+ 
 for(i=0;i<n;i++)
 for(j=0;j<n;j++)
 sp[i][j]=0;
@@ -56,13 +65,15 @@ key=1;
 i=n/2;
 j=(n/2)+1;
 temp=0;
-
+ 
 while(true)
 {
-   sp[i][j]=count;        
+//giving values in the array
+   sp[i][j]=count;       
  if(temp!=-1)
    dir=temp;
       switch(dir){
+
       case 0://up
            i--;
            break;
@@ -78,16 +89,17 @@ while(true)
       default:
               break;
               }
-
-   if(key>n)
+ 
+   if(key>n || i<0 || j<0)
    break;
+//getting the direction
    temp = direction(i,j,key);
    if((j-(n/2))>key)
    key++;
    count++;
 }
-
-
+ 
+//outputing
 for(i=0;i<n;i++){
 for(j=0;j<n;j++)
 cout<<std::setw(4)<<sp[i][j];
