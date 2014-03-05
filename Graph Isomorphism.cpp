c68215c86bc2d372ca52a8ef43f58a76b06402ed
Godly T.Alias <godlytalias@yahoo.co.in>
2014-03-05 13:26:16 +0530

    Graph Isomorphism - probability propn matrix test

--- Graph Isomorphism.cpp
+++ Graph Isomorphism.cpp
@@ -2,25 +2,22 @@
 #include<fstream>
 using namespace std;
 
-float *init_state; //initial state variable
 float *row_mat,*row_mat_copy;
 //returns the initial state distribution vector
-float* istate_dibn_vec(int i,int n)
+void istate_dibn_vec(float* init_state, int i,int n)
 {
-init_state = new float[n];
 for(int j=0;j<n;j++)
  if(j==i)
   init_state[j]=1.0;
  else
   init_state[j]=0.0;
-return init_state;
 }
 
 //computes the product of matrices m1 & m2 and write the result in res matrix
 template <typename T>
 void matrix_prod(T *res,T *m1,int c1,T **m2,int r2,int c2)
 {
-int sum;
+float sum;
 if(c1==r2){
  for(int j=0;j<c2;j++){
  sum=0;
@@ -30,7 +27,7 @@ if(c1==r2){
  }
 }}
 
-void prob_prop_matrix(float **p, float **g, int n, int initstate)
+float** prob_prop_matrix(float **p, float **g, int n, int initstate)
 {
 //dynamically allocating array for probability distribution matrix
 p = new float*[(2*n)-1];
@@ -39,8 +36,9 @@ p[i]=new float[n];
 //row_mat holds the value of each state distribution vector
 row_mat = new float[n];
 row_mat_copy = new float[n];
+//writes the initial state vector to the row_mat
+istate_dibn_vec(row_mat,initstate,n);
 
-row_mat = istate_dibn_vec(initstate,n);
 for(int i=0;i<((2*n)-1);i++)
 {
 //copying state distribution vector to probability propogation matrix
@@ -50,6 +48,8 @@ row_mat_copy[j]=row_mat[j];}
 //calculating the state distribution vector for string of next length
 matrix_prod(row_mat,row_mat_copy,n,g,n,n);
 }
+
+return p;
 }
 
 //returns the degree of a vertix
@@ -77,7 +77,7 @@ for(int i=0;i<n;i++){
 int main()
 {
 int i,j,n1=0,n2=0,deg=0;
-float **g1,**g2,**p1,**p2;
+float **g1,**g2,**p1=NULL,**p2=NULL;
 char ch;
 ch=' ';
 FILE *read1 = fopen("g1","r");
@@ -128,7 +128,15 @@ fscanf(read2,"%f",&g2[i][j]);
 prob_dibn(g1,n1);
 prob_dibn(g2,n2);
 
+p1=prob_prop_matrix(p1,g1,n1,1);
+p2=prob_prop_matrix(p2,g2,n2,1);
 
+
+for(i=0;i<((2*n1)-1);i++){
+for(j=0;j<n1;j++)
+cout<<p1[i][j]<<" ";
+cout<<"\n";
+}
 
 fclose(read1);
 fclose(read2);
@@ -142,15 +150,14 @@ for(i = 0; i < n2; i++) {
     delete [] g2[i];
 }
 delete [] g2;
-for(i = 0; i < n1; i++) {
+for(i = 0; i < (2*n1)-1; i++) {
     delete [] p1[i];
 }
 delete [] p1;
-for(i = 0; i < n2; i++) {
+for(i = 0; i < (2*n2)-1; i++) {
     delete [] p2[i];
 }
 delete [] p2;
-delete [] init_state;
 delete [] row_mat;
 delete [] row_mat_copy;
 
