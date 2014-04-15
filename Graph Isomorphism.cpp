d058b061d8a85c4da1ed089edc4eaebbc96980c1
Godly T.Alias <godlytalias@yahoo.co.in>
2014-04-15 23:53:31 +0530

    added permutation

--- Graph Isomorphism.cpp
+++ Graph Isomorphism.cpp
@@ -7,51 +7,46 @@
 
 #include<iostream>
 #include<fstream>
+#include<sys/stat.h>
 using namespace std;
 
-int n1=0,n2=0;
+const int MAX_PERMUTATIONS=1000;
 
+int n1,n2,perm;
 struct mapping
 {
 int map_ver;
-};
+float state;
+int classid; };
 
 mapping **map_g;
+float **g1,**g2;
+int node,w_node;
+int tmp_count;
 
-//check whether the two matrices m1 & m2 are equal
-bool eq_matrix(float **m1,float **m2)
-{
-//n1==n2
-for(int i=0;i<((2*n1)-1);i++)
- for(int j=0;j<n1;j++)
-  {
-   if(m1[i][map_g[0][j].map_ver]!=m2[i][map_g[1][j].map_ver]){
-    return false;}
-  } 
-return true;
-}
 
 //merge the partitions made by the mergesort
-void merge(float **a,int s1,int e1,int s2,int e2,int mat)
+void merge(float *g,int s1,int e1,int s2,int e2,int graph_id)
 {
-int i,j,l,temp;
+FILE *read;
+int i,j,temp;
+float a,b;
  if(s1<e2 && (e1+1)==s2)
  {
   i=s1;
   j=s2;
   while(i<=e1 && j<=e2)
   {
-   l=0;
-   while((a[l][map_g[mat][i].map_ver]==a[l][map_g[mat][j].map_ver]) && l<(n1-1))
-   l++;
-   if(a[l][map_g[mat][i].map_ver]<a[l][map_g[mat][j].map_ver])
+   a=g[map_g[graph_id][i].map_ver];
+   b=g[map_g[graph_id][j].map_ver];
+   if(a<b)
    i++;
-   else if(a[l][map_g[mat][i].map_ver]>=a[l][map_g[mat][j].map_ver])
+   else if(a>=b)
     {
-     temp = map_g[mat][j].map_ver;
+     temp = map_g[graph_id][j].map_ver;
      for(int k=j;k>i;k--)
-     map_g[mat][k].map_ver=map_g[mat][k-1].map_ver;
-     map_g[mat][i].map_ver=temp;
+     map_g[graph_id][k].map_ver=map_g[graph_id][k-1].map_ver;
+     map_g[graph_id][i].map_ver=temp;
      j++;
      i++; e1++;
     }
@@ -59,41 +54,55 @@ int i,j,l,temp;
  }
 }
 
-//mergesort the matrix a and write the mapping to g_map[mat]
-void mergesort(float **a,int start, int end,int mat)
+//mergesort the matrix a and write the mapping to map_g[mat]
+void mergesort(float *g,int start, int end,int graph_id)
 {
 if(start<(end-1))
  {
- mergesort(a,start,(start+end)/2,mat);
- mergesort(a,((start+end)/2)+1,end,mat);
- merge(a,start,(start+end)/2,((start+end)/2)+1,end,mat);
+ mergesort(g,start,(start+end)/2,graph_id);
+ mergesort(g,((start+end)/2)+1,end,graph_id);
+ merge(g,start,(start+end)/2,((start+end)/2)+1,end,graph_id);
  }
 else
- merge(a,start,start,end,end,mat);
+ merge(g,start,start,end,end,graph_id);
+ 
 }
 
 bool adj_mat_map(float **a1, float **a2)
 {
-//n1==n2
-for(int i=0;i<n1;i++)
- for(int j=0;j<n1;j++)
+int i,j;
+for(i=0;i<node;i++)
+ for(j=0;j<node;j++)
   if(a1[map_g[0][i].map_ver][map_g[0][j].map_ver]!=a2[map_g[1][i].map_ver][map_g[1][j].map_ver])
    return false;
 return true;
 }
 
-int isotest(float **p1,float **p2,float **a1,float **a2)
+
+int isotest(int p1_init_node,int p2_init_node,float **a1,float **a2)
 {
-mergesort(p1,0,n1-1,0);
-mergesort(p2,0,n2-1,1);
-if(eq_matrix(p1,p2))
- {
+    char filename[40];
+    sprintf(filename,"../graphiso/map_0_%d",p1_init_node);
+    FILE *read1 = fopen(filename,"r");
+    sprintf(filename,"../graphiso/map_1_%d",p2_init_node);
+    FILE *read2 = fopen(filename,"r");
+while(!feof(read1)){
+for(int i=0;i<node;i++)
+ fscanf(read1,"%d",&map_g[0][i].map_ver);
+
+while(!feof(read2)){
+for(int j=0;j<node;j++)
+ fscanf(read2,"%d",&map_g[1][j].map_ver);
+
   if(adj_mat_map(a1,a2))
-   return 2;
-  else
-   return 1;
- }
-else
+ { fclose(read1);
+   fclose(read2);
+   return 2; } }
+      
+ fseek(read2,0,SEEK_SET);
+    }
+fclose(read1);
+fclose(read2);
  return 0;
 }
 
@@ -109,8 +118,7 @@ for(int j=0;j<n;j++)
 }
 
 //computes the product of matrices m1 & m2 and write the result in res matrix
-template <typename T>
-void matrix_prod(T *res,T *m1,int c1,T **m2,int r2,int c2)
+void matrix_prod(float *res,float *m1,int c1,float **m2,int r2,int c2)
 {
 float y,t,c;
 if(c1==r2){
@@ -126,32 +134,142 @@ if(c1==r2){
  }
 }}
 
-//calculates the probability propogation matrix for the initial state initstate
-float** prob_prop_matrix(float **p, float **g, int n, int initstate)
+//swaps the given parameters
+void swap(mapping *a,mapping *b)
 {
-//dynamically allocating array for probability distribution matrix
-p = new float*[(2*n)-1];
-for(int i=0;i<((2*n)-1);i++)
-p[i]=new float[n];
+ mapping temp;
+ temp=*a;
+ *a=*b;
+ *b=temp;     
+}
+
+//output all the possible mappings when 2 column vectors of 
+//probability propogation matrix becomes equal
+void permute(int start,int end,FILE *file,int graph_id,bool flag)
+{
+// if(perm<MAX_PERMUTATIONS){
+ int t_start=0,t_end=0;
+ if(start==end && flag)
+ {
+ perm++;
+ for(int i=0;i<node;i++)
+  fprintf(file,"%d ",map_g[graph_id][i].map_ver);
+ fprintf(file,"\n");
+ }
+ else
+ {
+ for(int i=0;i<=(end-start);i++)
+  {
+   swap(&map_g[graph_id][start],&map_g[graph_id][start+i]);
+   if((start==0 || start>0 && map_g[graph_id][start].classid!=map_g[graph_id][start-1].classid || i!=0)&& end<(node-1))
+   {
+   for(int j=end+1;j<(node-1);j++)
+    if(map_g[graph_id][j].classid==map_g[graph_id][j+1].classid)
+     { t_start=j; break; }
+   if(t_start>0)
+   for(int j=t_start+1;j<node-1;j++)
+    if(map_g[graph_id][j].classid!=map_g[graph_id][j+1].classid) //no need to check for last element in the row as it will always be different
+     { t_end = j; break; }
+   if(t_start!=t_end){
+     permute(t_start,t_end,file,graph_id,true);
+     flag=false;}}
+   permute(start+1,end,file,graph_id,flag);
+   flag=true;
+   swap(&map_g[graph_id][start+i],&map_g[graph_id][start]);
+   }
+ }
+}//}
+
+//calculates the probability propogation matrix for the initial state initstate
+void prob_prop_matrix(int graph_id, float **g, int n, int initstate)
+{
+char file_name[40];
+bool flag=true;
+int start,end,j,temp,classptr;
+float temps;
+sprintf(file_name,"../graphiso/map_%d_%d",graph_id,initstate);
+FILE *write = fopen(file_name,"w");
 //row_mat holds the value of each state distribution vector
 row_mat = new float[n];
 row_mat_copy = new float[n];
 //writes the initial state vector to the row_mat
 istate_dibn_vec(row_mat,initstate,n);
-
-for(int i=0;i<((2*n)-1);i++)
+classptr=1;
+for(int i=0;flag && i<((2*n)-1);i++)
 {
-//copying state distribution vector to probability propogation matrix
-for(int j=0;j<n;j++){
-p[i][j]=row_mat[j];
-row_mat_copy[j]=row_mat[j];}
+        j=1;
+        //this loop gives different class id to vertices with same class id but different state
+        while(j<n)
+        {
+        if(map_g[graph_id][j].classid==map_g[graph_id][j-1].classid)
+        {
+         if(map_g[graph_id][j].state!=map_g[graph_id][j-1].state)
+          {
+            temp=map_g[graph_id][j].classid;
+          while(j<n && map_g[graph_id][j].classid==temp){
+            temps=map_g[graph_id][j].state;
+             while(j<n && map_g[graph_id][j].state==temps && map_g[graph_id][j].classid==temp){
+              map_g[graph_id][j].classid=classptr;
+              j++;
+              }
+              classptr++;
+              }  
+             }  
+          else j++;
+             }
+         else j++;
+                  }
+                  
+        start=0;
+        j=0;
+        flag=false;
+        while(j<n)
+        {
+        end=start+1;
+        j++;
+        while(j<n && map_g[graph_id][end].classid==map_g[graph_id][start].classid)
+        {
+          j++; end++;               
+         }
+        if(start<end-1){
+        mergesort(row_mat,start,end-1,graph_id);
+        flag=true;}
+                
+        start=end;
+        }
+       
+//writing state distribution vector to probability propogation matrix
+for(j=0;j<n;j++){
+ row_mat_copy[j]=row_mat[j];
+ map_g[graph_id][j].state=row_mat[map_g[graph_id][j].map_ver];
+ }
+ 
 //calculating the state distribution vector for string of next length
 matrix_prod(row_mat,row_mat_copy,n,g,n,n);
 }
 
 delete [] row_mat;
 delete [] row_mat_copy;
-return p;
+start=0;
+while(start<n-1){
+ if(map_g[graph_id][start].classid==map_g[graph_id][start+1].classid)
+  break;
+ start++; }
+end=start+1;
+while(end<n-1){
+ if(map_g[graph_id][end].classid!=map_g[graph_id][end+1].classid)
+  break;
+ end++; }
+ perm=0;
+if(start<end && end<n)
+ permute(start,end,write,graph_id,true);
+else
+{
+for(int i=0;i<node;i++)
+  fprintf(write,"%d ",map_g[graph_id][i].map_ver);
+ fprintf(write,"\n");   
+}
+fclose(write);
 }
 
 //returns the degree of a vertix
@@ -159,7 +277,7 @@ int degree(float **m,int row,int n)
 {
 int deg=0;
 for(int i=0;i<n;i++)
-deg+=m[row][i];
+deg+=(int)m[row][i];
 return deg;
 }
 
@@ -176,53 +294,44 @@ for(int i=0;i<n;i++){
 }
 }
 
-int main()
+void get_graphs()
 {
-int i,j,mode,deg=0,pi,pj,iso=0;
-float **g1,**g2,**p1=NULL,**p2=NULL;
-char ch;
-ch=' ';
+     int mode=0;
+     char ch=' ';
+     FILE *f = fopen("g1.txt","r");
+      n1=0;n2=0;
+         //checking the no: of nodes in the graph 1
+		 while(ch!='\n')
+         {
+         ch = fgetc(f);
+          if(ch>=48 && ch<=57 && mode==0)
+           {
+             mode=1;
+               n1++;
+            }
+             else if(ch<48 || ch>57)
+              mode=0;
+          }
+          
+          //initializing graph 1 and inputing values
+         g1 = new float*[n1];
+         for(int i=0;i<n1;i++)
+         g1[i]=new float[n1];
+         fseek(f,0,SEEK_SET);
+         for(int i=0;i<n1;i++)
+         for(int j=0;j<n1;j++)
+         fscanf(f,"%f",&g1[i][j]);
+         
+         fclose(f);
+         
+         ch=' ';
 mode=0;
-FILE *read1 = fopen("g1","r");
-//reading the adjacent matrix of Graph 1
-//first checking the no: of elements in a row
-while(ch!='\n')
-{
-ch = fgetc(read1);
- if(ch>=48 && ch<=57 && mode==0)
- {
-  mode=1;
-  n1++;
- }
- else if(ch<48 || ch>57)
-  mode=0;
-}
-
-//dynamically allocating array
-map_g = new mapping*[2];
-map_g[0]=new mapping[n1];
-
-g1 = new float*[n1];
-for(i=0;i<n1;i++){
-g1[i]=new float[n1];
-map_g[0][i].map_ver=i;
-}
-
-fseek(read1,0,SEEK_SET);
-for(i=0;i<n1;i++)
-for(j=0;j<n1;j++){
-fscanf(read1,"%f",&g1[i][j]);
-}
-
-fclose(read1);
-ch=' ';
-mode=0;
-FILE *read2 = fopen("g2","r");
+f = fopen("g2.txt","r");
 //reading the adjacent matrix of Graph 2
 //first checking the no: of elements in a row
 while(ch!='\n')
 {
-ch = fgetc(read2);
+ch = fgetc(f);
  if(ch>=48 && ch<=57 && mode==0)
  {
   mode=1;
@@ -231,61 +340,97 @@ ch = fgetc(read2);
  else if(ch==' ')
   mode=0;
 }
-
-//dynamically allocating array
-map_g[1]=new mapping[n2];
-
-g2 = new float*[n2];
-
-for(i=0;i<n2;i++){
-g2[i]=new float[n2];
-map_g[1][i].map_ver = i;
-}
-
-fseek(read2,0,SEEK_SET);
-for(i=0;i<n2;i++)
-for(j=0;j<n2;j++){
-fscanf(read2,"%f",&g2[i][j]);
-}
-
-fclose(read2);
-//computing probability distribution matrices of both graphs
+         g2 = new float*[n2];
+         for(int i=0;i<n2;i++)
+         g2[i]=new float[n2];
+         fseek(f,0,SEEK_SET);
+         for(int i=0;i<n2;i++)
+         for(int j=0;j<n2;j++)
+         fscanf(f,"%f",&g2[i][j]);
+         
+         fclose(f); 
+         
+         //computing probability distribution matrices of both graphs
 prob_dibn(g1,n1); //g1 is converted to the probability distribution matrix of graph 1
-prob_dibn(g2,n2); //g2 is converted to the probability distribution matrix of graph 2
+prob_dibn(g2,n2); //g2 is converted to the probability distribution matrix of graph 2          
+     }
+
+int main()
+{
+    #if defined(_WIN32)
+    _mkdir("../graphiso");
+    _mkdir("../results");
+    #else
+    mkdir("../graphiso", 0777);
+    mkdir("../results",0777);
+    #endif
+
+FILE *result;
+
+int i,j,mode,deg=0,pi,pj,iso;
+char ch,filename[40];
+ch=' ';
+get_graphs();
+
+map_g = new mapping*[2];
+for(int i=0;i<2;i++)
+map_g[i]=new mapping[n1];
+
+
+FILE *read1,*read2;
 
 if(n1==n2) //if number of vertices of both graphs are not equal then not isomorphic
 {
- iso=0;
+           node=n1;
+ //dynamically allocating array for probability distribution matrix
  for(pi=0;(pi<n1)&&(iso!=2);pi++)
  {
-  p1=prob_prop_matrix(p1,g1,n1,pi);
+        for(int i=0;i<n1;i++)
+        {
+        map_g[0][i].map_ver=i;
+        map_g[0][i].state=-1.0;
+        map_g[0][i].classid=0;
+        }
+  sprintf(filename,"../graphiso/map_%d_%d",0,pi);  
+  read1=fopen(filename,"r");
+  if(!read1)
+   prob_prop_matrix(0,g1,n1,pi);
+   else
+  fclose(read1);
   for(pj=0;(pj<n2)&&(iso!=2);pj++)
   {
-   p2=prob_prop_matrix(p2,g2,n2,pj);
-   iso = isotest(p1,p2,g1,g2);
-   //deleting the memory for the probability propogation matrix
-   for(i = 0; i < (2*n2)-1; i++) {
-    delete [] p2[i];
-   }
-   delete [] p2;
+   for(int i=0;i<n1;i++)
+        {
+        map_g[1][i].map_ver=i;
+        map_g[1][i].state=-1.0;
+        map_g[1][i].classid=0;
+        }
+   sprintf(filename,"../graphiso/map_%d_%d",1,pj);
+   read2=fopen(filename,"r");
+   if(!read2)
+    prob_prop_matrix(1,g2,n2,pj);
+    else
+   fclose(read2);
+   iso = isotest(pi,pj,g1,g2);
   }
-  //deleting the memory for probability propogation matrix
-  for(i = 0; i < (2*n1)-1; i++) {
-    delete [] p1[i];
-  }
-  delete [] p1;
+  
+  if(iso==2)
+{
+sprintf(filename,"../results/res_%d_%d",pi,pj);
+result=fopen(filename,"w");
+fprintf(result,"ISOMORPHIC MAPPING\n");
+for(int l=0;l<n1;l++)
+fprintf(result,"%d -> %d\n",map_g[0][l].map_ver,map_g[1][l].map_ver);
+fprintf(result,"\n----------------\n");
+fclose(result);
+}
+  
  }
 }
 else
- iso=0;
+cout<<"NOT ISOMORPHIC\n";
 
-if(iso==2)
-{
-cout<<"ISOMORPHIC MAPPING\n";
-for(i=0;i<n1;i++)
-cout<<map_g[0][i].map_ver<<"->"<<map_g[1][i].map_ver<<"\n";
-}
-else
+if(iso!=2)
 cout<<"NOT ISOMORPHIC\n";
 
 //deleting memory allocated for arrays
@@ -301,6 +446,5 @@ for(i=0;i<=1;i++)  {
     delete [] map_g[i];
 }
 delete [] map_g;
-
 return 0;
 }
